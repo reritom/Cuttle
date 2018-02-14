59786eb506e3d83f5d88da0cddcedf0a99298ef7
Tomas Sheers <reikudjinn@gmail.com>
2018-02-14 10:03:37 +0100

    Create fin.py

---
+++ simulator/fin.py
@@ -0,0 +1,11 @@
+'''
+  Fins are used to make up the tracks.
+  It has two binary inputs
+  The output is the position and is time dependent.
+  If both inputs are 1, it will break
+  If both are zero, its neutral
+  If it is XOR, the fin will either move up or down
+'''
+class Fin():
+  __init__(self):
+    pass
